aae6c7405d50fdfadd8d73049a851b417382ec93
lzzzam <58335995+lzzzam@users.noreply.github.com>
2019-12-13 15:14:14 +0100

    Changed driver location in STM32F303RE_driver project

--- STM32_Workspace/STM32F303RE_driver/Debug/Src/main.d
+++ STM32_Workspace/STM32F303RE_driver/Debug/Src/main.d
@@ -1,4 +1,4 @@
 Src/main.o: ../Src/main.c \
- C:/Users/Luca\ Zambuto/Desktop/STM32\ Nucleo\ Board/STM32_Workspace/Embedded-Software/inc/STM32F303RE.h
+ C:/Users/Luca\ Zambuto/Desktop/STM32\ Nucleo\ Board/Embedded-Software/STM32_Workspace/STM32F303RE_driver/driver/inc/STM32F303RE.h
 
-C:/Users/Luca\ Zambuto/Desktop/STM32\ Nucleo\ Board/STM32_Workspace/Embedded-Software/inc/STM32F303RE.h:
+C:/Users/Luca\ Zambuto/Desktop/STM32\ Nucleo\ Board/Embedded-Software/STM32_Workspace/STM32F303RE_driver/driver/inc/STM32F303RE.h:
