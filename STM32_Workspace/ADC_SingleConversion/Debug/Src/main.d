96d58d78704400ad94497e71e311991378772f5d
lzzzam <58335995+lzzzam@users.noreply.github.com>
2019-12-13 19:04:42 +0100

    Separated driver directory from Eclipse Workspace

--- STM32_Workspace/ADC_SingleConversion/Debug/Src/main.d
+++
@@ -1,3 +0,0 @@
-Src/main.o: ../Src/main.c ../Src/STM32F303RE.h
-
-../Src/STM32F303RE.h:
--- STM32_Workspace/STM32F303RE_driver/Debug/Src/main.d
+++
@@ -1,4 +0,0 @@
-Src/main.o: ../Src/main.c \
- C:/Users/luca/Documents/GitHub/Embedded-Software/STM32_Workspace/STM32F303RE_driver/driver/inc/STM32F303RE.h
-
-C:/Users/luca/Documents/GitHub/Embedded-Software/STM32_Workspace/STM32F303RE_driver/driver/inc/STM32F303RE.h:
