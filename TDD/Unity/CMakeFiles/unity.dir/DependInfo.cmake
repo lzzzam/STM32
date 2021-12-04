1a8abd40b8ce6dca93d76818e9f3eac96ae6c2b5
lzzzam <58335995+lzzzam@users.noreply.github.com>
2021-12-05 00:31:30 +0100

    Added TDD project and ARM Toolchain

---
+++ TDD/Unity/CMakeFiles/unity.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/luca/Documents/GitHub/Embedded-Software/TDD/Unity/src/unity.c" "/Users/luca/Documents/GitHub/Embedded-Software/TDD/Unity/CMakeFiles/unity.dir/src/unity.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
